4962571486bb38d013e59421369db408d3c2a8f3
takahiro.sugaya <takskg@gmail.com>
2020-05-06 23:06:56 +0900

    add:.nesファイルのヘッダーを読み込む所まで

--- nes-emulator/src/main.rs
+++ nes-emulator/src/main.rs
@@ -1,3 +1,15 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+mod nes;
+
+
 fn main() {
-    println!("Hello, world!");
+    //NESファイルからROMデータを読み込み
+    let mut file = File::open("rom/hello-world.nes").unwrap();
+    let mut buffer: Vec<u8> = Vec::new();
+    file.read_to_end(&mut buffer).unwrap();
+
+    let mut nes_rom: nes::Nes = Default::default();
+    nes_rom.load(buffer);
 }
---
+++ nes-emulator/src/nes.rs
@@ -0,0 +1,55 @@
+﻿
+pub struct Header {
+    prom_size: u8,
+    crom_size: u8,
+    flag6: u8,
+    flag7: u8,
+    flag8: u8,
+    flag9: u8,
+    flag10: u8,
+}
+impl Default for Header {
+    fn default() -> Self {
+        Self {
+            prom_size: 0,
+            crom_size: 0,
+            flag6: 0,
+            flag7: 0,
+            flag8: 0,
+            flag9: 0,
+            flag10: 0,
+        }
+    }
+}
+
+pub struct Nes {
+    pub header: Header,
+}
+
+impl Default for Nes {
+    fn default() -> Self {
+        Self {
+            header: Default::default(),
+        }
+    }
+}
+
+impl Nes {
+    pub fn load(&mut self, buffer: Vec<u8>) -> bool {
+        let mut result = false;
+
+        if buffer[0] == b'N' && buffer[1] == b'E' && buffer[2] == b'S' {
+            self.header.prom_size = buffer[3];
+            self.header.crom_size = buffer[4];
+            self.header.flag6 = buffer[5];
+            self.header.flag7 = buffer[6];
+            self.header.flag8 = buffer[7];
+            self.header.flag9 = buffer[8];
+            self.header.flag10 = buffer[9];
+
+            result = true;
+        }
+        
+        return result;
+    }
+}

ed65dfb96c73f36d3ed9d3af16cf0f387c3a3ea2
takahiro.sugaya <takskg@gmail.com>
2020-05-10 01:53:47 +0900

    add:ロガーにenv_loggerを導入した

--- nes-emulator/src/main.rs
+++ nes-emulator/src/main.rs
@@ -1,10 +1,22 @@
 use std::fs::File;
 use std::io::prelude::*;
 
+#[macro_use]
+extern crate log;
+
 mod nes;
 use nes::cassette::Cassette;
 
+fn logger_initialize() {
+    //とりあえず全部出す
+    std::env::set_var("RUST_LOG", "trace");
+    env_logger::init();
+}
+
 fn main() {
+    //ロガーの設定
+    logger_initialize();
+
     //NESファイルからROMデータを読み込み
     let mut file = File::open("rom/hello-world.nes").unwrap();
     let mut buffer: Vec<u8> = Vec::new();
@@ -12,8 +24,8 @@ fn main() {
 
     let mut cst: Cassette = Default::default();
     if cst.load_from_buffer(buffer) {
-        println!("NESファイルの読み込みに成功！！");
+        info!("NESファイルの読み込みに成功！！");
     }else{
-        println!("NESファイルの読み込みに失敗しました");
+        info!("NESファイルの読み込みに失敗しました");
     }
 }
